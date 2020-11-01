5d3990ecf79d8547195182921d5ad0e831bc6f86
Bogdan Popa <bogdan@defn.io>
2020-11-01 23:51:09 +0200

    vm: clean up some of the implementation

--- wasm-lib/private/vm.rkt
+++ wasm-lib/private/vm.rkt
@@ -3,31 +3,26 @@
 (require racket/contract
          racket/match
          "core.rkt"
-         "error.rkt")
+         "error.rkt"
+         "memory.rkt")
 
 (provide
  vm?
  make-vm
  vm-ref)
 
-(define PAGESIZE (* 64 1024))
-
-(struct vm (mod links pages)
+(struct vm (mod mem links)
   #:transparent)
-
-(define (make-memory-pages n)
-  (for/vector ([_ (in-range n)])
-    (make-vector PAGESIZE 0)))
 
 (define/contract (make-vm m links)
   (-> mod? (hash/c string? (hash/c string? procedure?)) vm?)
-  (define pages
+  (define mem
     (match (mod-memories m)
-      [(vector (memtype (limits lo _)))
-       (make-memory-pages lo)]
+      [(vector (memtype (limits min-pages max-pages)))
+       (make-memory min-pages max-pages)]
       [(vector)
-       (vector)]))
-  (vm m links pages))
+       #f]))
+  (vm m mem links))
 
 (define/contract (vm-ref v name)
   (-> vm? string? (or/c #f procedure?))
@@ -48,7 +43,6 @@
   (define code (vector-ref (mod-codes m) idx))
   (define instrs (code-instrs code))
   (define locals
-    ;; TOOD: Take type into account.
     (make-vector (+ (length args)
                     (for/sum ([l (in-vector (code-locals code))])
                       (locals-n l)))))
@@ -56,67 +50,62 @@
     (vector-set! locals posn arg))
   (vm-exec v instrs locals))
 
-(define (vm-exec v instrs locals [stack null])
+(define (vm-exec v instrs locals)
   (define m (vm-mod v))
   (define fns (mod-functions m))
   (define types (mod-types m))
-  (define maxip (sub1 (vector-length instrs)))
-  (let loop ([ip 0] [stack stack])
+  (for/fold ([stack null])
+            ([instr (in-vector instrs)])
     #;(printf "ip: ~s instr: ~.s stack: ~s~n" ip (vector-ref instrs ip) stack)
-    (define new-stack
-      (match (vector-ref instrs ip)
-        [(instr:local.get idx)
-         (cons (vector-ref locals idx) stack)]
+    (match instr
+      [(instr:unreachable)
+       (trap "unreachable")]
 
-        [(instr:local.set idx)
-         (vector-set! locals idx (car stack))
-         (cdr stack)]
+      [(instr:nop)
+       null]
 
-        [(instr:local.tee idx)
-         (begin0 stack
-           (vector-set! locals idx (car stack)))]
+      [(instr:local.get idx)
+       (cons (vector-ref locals idx) stack)]
 
-        [(instr:drop)
-         (cdr stack)]
+      [(instr:local.set idx)
+       (vector-set! locals idx (car stack))
+       (cdr stack)]
 
-        [(instr:select)
-         (match stack
-           [(list* 0 v2 v1 stack-remainder) (cons v1 stack-remainder)]
-           [(list* _ v2 v1 stack-remainder) (cons v2 stack-remainder)])]
+      [(instr:local.tee idx)
+       (begin0 stack
+         (vector-set! locals idx (car stack)))]
 
-        [(instr:nop)
-         stack]
+      [(instr:drop)
+       (cdr stack)]
 
-        [(instr:unreachable)
-         (trap "unreachable")]
+      [(instr:select)
+       (match stack
+         [(list* 0 _ v stack-remainder) (cons v stack-remainder)]
+         [(list* _ v _ stack-remainder) (cons v stack-remainder)])]
 
-        [(instr:i32.const n)
-         (cons n stack)]
+      [(instr:i32.const n)
+       (cons n stack)]
 
-        [(instr:i32.gt_s)
-         (cons (if (> (cadr stack) (car stack)) 1 0) (cddr stack))]
+      [(instr:i32.gt_s)
+       (cons (if (> (cadr stack) (car stack)) 1 0) (cddr stack))]
 
-        [(instr:i32.sub)
-         (cons (- (cadr stack) (car stack)) (cddr stack))]
+      [(instr:i32.sub)
+       (cons (- (cadr stack) (car stack)) (cddr stack))]
 
-        [(instr:i32.add)
-         (cons (+ (cadr stack) (car stack)) (cddr stack))]
+      [(instr:i32.add)
+       (cons (+ (cadr stack) (car stack)) (cddr stack))]
 
-        [(instr:if _ then-b else-b)
-         (match stack
-           [(cons 0 stack-remainder) (vm-exec v else-b locals stack-remainder)]
-           [(cons _ stack-remainder) (vm-exec v then-b locals stack-remainder)])]
+      [(instr:if _ then-b else-b)
+       (match stack
+         [(cons 0 _) (vm-exec v else-b locals)]
+         [(cons _ _) (vm-exec v then-b locals)])]
 
-        [(instr:call funcidx)
-         (define typeidx (vector-ref fns funcidx))
-         (define type (vector-ref types typeidx))
-         (define-values (args stack-remainder)
-           (split-at stack (length (functype-params type))))
-         (append (vm-apply v funcidx args) stack-remainder)]))
-
-    (if (< ip maxip)
-        (loop (add1 ip) new-stack)
-        new-stack)))
+      [(instr:call funcidx)
+       (define typeidx (vector-ref fns funcidx))
+       (define type (vector-ref types typeidx))
+       (define-values (args stack-remainder)
+         (split-at stack (length (functype-params type))))
+       (append (vm-apply v funcidx args) stack-remainder)])))
 
 (define (split-at ys pos)
   (for/fold ([xs null] [ys ys]
@@ -124,13 +113,3 @@
             ([_ (in-range pos)])
     (values (cons (car ys) xs)
             (cdr ys))))
-
-(define i32?
-  (integer-in
-   (- (expt 2 31))
-   (sub1 (expt 2 32))))
-
-(define i64?
-  (integer-in
-   (- (expt 2 63))
-   (sub1 (expt 2 64))))
