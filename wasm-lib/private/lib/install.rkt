891d271335fa40565fc9e4fa003577487cd4f88d
Bogdan Popa <bogdan@defn.io>
2020-11-11 12:52:30 +0200

    lib: fallback to gcc and cc on install

--- wasm-lib/private/lib/install.rkt
+++ wasm-lib/private/lib/install.rkt
@@ -5,11 +5,15 @@
 
 (provide pre-installer)
 
+(define cc
+  (for/first ([name '("clang" "gcc" "cc")])
+    (find-executable-path name)))
+
 (define (pre-installer _collections-top-path this-collection-path)
   (define lib-path (build-path this-collection-path "private" "lib"))
   (define source-path (build-path lib-path "runtime.c"))
   (define output-path (build-path lib-path (append-extension-suffix "runtime")))
   (parameterize ([current-directory lib-path])
-    (system* (find-executable-path "clang")
+    (system* cc
              "-Wall" "-Werror" "-O3" "-fPIC" "-shared"
              "-o" output-path source-path)))
