b1eae7e7e6c59d54493c9fdde62623fbd7c39778
Bogdan Popa <bogdan@defn.io>
2020-11-12 14:53:51 +0200

    binary: improve performance of reading exprs

--- wasm-lib/private/location.rkt
+++ wasm-lib/private/location.rkt
@@ -2,22 +2,7 @@
 
 (provide
  (struct-out loc)
- (struct-out loc-unknown)
- track-source-location?
- port-loc)
-
-(define track-source-location?
-  (make-parameter #f))
+ (struct-out loc-unknown))
 
 (struct loc (source position))
 (struct loc-unknown (source))
-
-(define (port-loc in)
-  (cond
-    [(track-source-location?)
-     (define-values (_line _column pos)
-       (port-next-location in))
-     (loc (object-name in) pos)]
-
-    [else
-     (loc-unknown (object-name in))]))
